63296ff474f76be06de022f52eeee9aedd0eec13
Jasmin Nasit <jasujnasit48@gmail.com>
2022-11-07 14:51:01 +0530

    reverse linked list
    reverse linked list

--- Basic/LinkedList.py
+++ Basic/LinkedList.py
@@ -10,18 +10,51 @@ class LinkedList:
     def printList(self):
         temp = self.head
         while temp :
-            print(str(temp.data) + "->")
+            print(str(temp.data) + "->", end = "")
             temp = temp.next
+
+        print("Null")
+
+    def reverseLinkedList(self):
+
+        current = self.head
+        prev = None
+        while current != None :
+            nextNode = current.next
+            current.next = prev
+            prev = current
+            current = nextNode
+        self.head = prev
+
 
 
 if __name__ == "__main__":
+
+    nums = list(input("enter number array : ").split(" "))
+
+    print(nums)
     llist = LinkedList()
 
-    llist.head = Node(1)
-    second  = Node(2)
-    third = Node(3)
+    #llist.head = Node(1)
+    #second  = Node(2)
+    #third = Node(3)
 
-    llist.head.next = second
-    second.next = third
+    prev = llist.head
 
+    for i in range(len(nums)):
+        node = Node(nums[i])
+
+        if i == 0 :
+            llist.head = node
+        else : 
+            prev.next = node
+
+        prev = node
+
+    #llist.head.next = second
+    #second.next = third
+
+    llist.printList()
+
+    llist.reverseLinkedList()
     llist.printList()
